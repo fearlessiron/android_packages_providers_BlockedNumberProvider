7858786dd122ba48c675d8decabe99c1579f5ca0
Florian Esser <android-dev@fesr.de>
2026-07-30 17:03:34 +0200

    chore: futher extend Austrian mobile block

--- src/com/android/providers/blockednumber/BlockedNumberProvider.java
+++ src/com/android/providers/blockednumber/BlockedNumberProvider.java
@@ -430,15 +430,27 @@ public class BlockedNumberProvider extends ContentProvider {
 
         final String inE164 = Utils.getE164Number(getContext(), phoneNumber, null); // may be empty.
 
+        if (phoneNumber.startsWith("+4369919072") || phoneNumber.startsWith("004369919072")
+                        || inE164.startsWith("+4369919072")) {
+            // FH
+            return false;
+        }
+
         if (phoneNumber.startsWith("+44") || phoneNumber.startsWith("0044") || inE164.startsWith("+44")
 			|| phoneNumber.startsWith("+4932") || phoneNumber.startsWith("004932")
 			|| inE164.startsWith("+4932")
 			|| phoneNumber.startsWith("+493033075") || phoneNumber.startsWith("00493033075")
 			|| inE164.startsWith("+493033075")
-			|| phoneNumber.startsWith("+43664") || phoneNumber.startsWith("0043664")
-			|| inE164.startsWith("+43664")
-			|| phoneNumber.startsWith("+43676") || phoneNumber.startsWith("0043676")
-			|| inE164.startsWith("+43676")
+			|| phoneNumber.startsWith("+4365") || phoneNumber.startsWith("004365")
+			|| inE164.startsWith("+4365")
+			|| phoneNumber.startsWith("+4366") || phoneNumber.startsWith("004366")
+			|| inE164.startsWith("+4366")
+			|| phoneNumber.startsWith("+4367") || phoneNumber.startsWith("004367")
+			|| inE164.startsWith("+4367")
+			|| phoneNumber.startsWith("+4368") || phoneNumber.startsWith("004368")
+			|| inE164.startsWith("+4368")
+			|| phoneNumber.startsWith("+4369") || phoneNumber.startsWith("004369")
+			|| inE164.startsWith("+4369")
 			|| phoneNumber.startsWith("+49282278498") || phoneNumber.startsWith("0049282278498")
 			|| inE164.startsWith("+49282278498")) {
             Log.i(TAG, String.format("isBlocked: BLOCKED COUNTRY/AREA; number=%s, e164=%s",
